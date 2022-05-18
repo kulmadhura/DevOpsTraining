4ca291761f91e3b3ef65591841e333399380b884
Madhura Kulkarni <kulkarnimadhura@gmail.com>
2022-05-18 11:24:25 +0530

    first commit

---
+++ src/main/java/com/citi/simpleProject/Application.java
@@ -0,0 +1,18 @@
+package com.citi.simpleProject;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Application main class.
+ */
+@Log4j2
+public class Application {
+
+
+    public static Application INSTANCE;
+
+    public static void main(final String[] args) {
+    	System.out.println("Print message");
+    }
+
+}
